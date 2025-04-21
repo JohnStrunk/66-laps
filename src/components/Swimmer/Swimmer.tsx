85992719c44ee5c35c70847e53169366140e67fa
John Strunk <john.strunk@gmail.com>
2025-04-21 23:37:37 +0000

    Fix emoji font
    Import Noto Color Emoji font to ensure proper rendering of emojis in the
    Swimmer component, and update the TextStyle to use the imported font.
    
    Signed-off-by: John Strunk <john.strunk@gmail.com>

--- src/components/Swimmer/Swimmer.tsx
+++ src/components/Swimmer/Swimmer.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { ISwimmer } from "@/modules/SwimmerModel";
 import { extend, useTick } from "@pixi/react";
+import { Noto_Color_Emoji } from "next/font/google";
 import { Container, Graphics, GraphicsContext, PointData, Sprite, Text, TextStyle } from "pixi.js";
 import { useState } from "react";
 
@@ -12,6 +13,16 @@ const emojis = [
     "🔱",
     "🩴",
 ];
+
+// Not entirely sure if this is the right way to do this, but it seems to help
+// get the right emoji font. If the font hasn't loaded by the time the
+// component is rendered, it will use the default font.
+export const noto_color_emoji = Noto_Color_Emoji({
+    weight: '400',
+    subsets: ['emoji'],
+    preload: true,
+    display: 'swap',
+});
 
 export type SwimmerProps = {
     /** Coordinates of the end wall at the start end of the pool */
@@ -50,7 +61,7 @@ export default function Swimmer(props: SwimmerProps) {
             style={new TextStyle({
                 fontSize: props.laneWidth * 0.5,
                 fill: "white", // Text color
-                fontFamily: "Noto Color Emoji, sans-serif",
+                fontFamily: "Noto Color Emoji",
                 fontStyle: "normal",
             })}
             position={usePosition}
