17e632eb5742372c69b29a509f7820a0f6551719
John Strunk <john.strunk@gmail.com>
2025-05-03 00:43:22 +0000

    Add counting guide
    Updated pre-commit config to exclude media files, renamed howto layout and
    page to counting, and added a new counting guide with images and
    instructions.
    
    Signed-off-by: John Strunk <john.strunk@gmail.com>

--- src/app/counting/layout.tsx
+++ src/app/counting/layout.tsx
@@ -1,6 +1,6 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="prose prose-h1:prose-2xl p-6 max-w-3xl">
+        <div className="prose prose-h1:prose-2xl p-6 max-w-3xl mx-auto">
             {children}
         </div>
     )
