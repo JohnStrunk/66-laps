47ac228f01842b6ce687c29940f52c3e19b013c8
John Strunk <john.strunk@gmail.com>
2025-05-06 23:05:40 +0000

    Add lap counting sheets
    Added custom lap counting sheets for SC 500, SC 1000/1650, and LC 800/1500,
    along with links to external sheets from USA Swimming. Installed lucide-
    react for icons and updated globals.css with custom styling for lucide
    icons.
    
    Signed-off-by: John Strunk <john.strunk@gmail.com>

---
+++ src/app/sheets/page.tsx
@@ -0,0 +1,82 @@
+'use client'
+
+import { Divider, Link } from "@heroui/react";
+import { ExternalLink, FileText } from "lucide-react";
+
+type LapCountingSheet = {
+    name: string;
+    url: string;
+}
+
+const externalSheets: LapCountingSheet[] = [
+    {
+        name: "USA Swimming SC 500",
+        url: "https://websitedevsa.blob.core.windows.net/sitefinity/docs/default-source/officialsdocuments/officiating-forms/split-recording-forms/500-yard-freestyle-split-recording-sheet.pdf",
+    },
+    {
+        name: "USA Swimming SC 1000",
+        url: "https://websitedevsa.blob.core.windows.net/sitefinity/docs/default-source/officialsdocuments/officiating-forms/split-recording-forms/1000-yard-freestyle-split-recording-sheet.pdf",
+    },
+    {
+        name: "USA Swimming SC 1650",
+        url: "https://websitedevsa.blob.core.windows.net/sitefinity/docs/default-source/officialsdocuments/officiating-forms/split-recording-forms/1650-yard-freestyle-split-recording-sheet.pdf",
+    },
+    {
+        name: "USA Swimming LC 800",
+        url: "https://websitedevsa.blob.core.windows.net/sitefinity/docs/default-source/officialsdocuments/officiating-forms/split-recording-forms/800-free-lcb91036fa6cbc6a0a9b57ff00009030c2.pdf",
+    },
+    {
+        name: "USA Swimming LC 1500",
+        url: "https://websitedevsa.blob.core.windows.net/sitefinity/docs/default-source/officialsdocuments/officiating-forms/split-recording-forms/1500-meter-freestyle-lc-split-recording-sheet.pdf",
+    },
+];
+
+export default function Page() {
+    return (
+        <div className="prose mx-auto p-6">
+            <h1>Lap counting sheets</h1>
+            <h2 className="text-lg mb-2">66-Laps custom counting sheets</h2>
+            <p className="text-sm">
+                Want to customize these sheets? Check out the{' '}
+                <Link
+                    isExternal
+                    showAnchorIcon
+                    href="https://docs.google.com/spreadsheets/d/1GHPSscX-hn9-Av33UEZ2iyQLtknGxFwlX46lGB50Xc8/edit?usp=sharing"
+                    className="text-sm leading-none">
+                    editable version.
+                </Link>
+            </p>
+            <ul className="list-none p-0">
+                <li>
+                    <Link href="/sheets/SC500.pdf">
+                        <FileText />&nbsp;66-Laps SC 500
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/sheets/SC1000-1650.pdf">
+                        <FileText />&nbsp;66-Laps SC 1000/1650
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/sheets/LC800-1500.pdf">
+                        <FileText />&nbsp;66-Laps LC 800/1500
+                    </Link>
+                </li>
+            </ul>
+            <Divider />
+            <h2 className="text-lg mb-2">Other counting sheets</h2>
+            <p className="text-sm">
+                The following counting sheets are provided for convenience and are unaffiliated with 66-Laps. All rights belong to their respective owners.
+            </p>
+            <ul className="list-none p-0 md:grid md:grid-cols-2">
+                {externalSheets.map((sheet) => (
+                    <li key={sheet.url}>
+                        <Link isExternal href={sheet.url}>
+                            <ExternalLink />&nbsp;{sheet.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
