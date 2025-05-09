0de2a6c412c764fa4c17b1611cab99d07d448355
John Strunk <john.strunk@gmail.com>
2025-05-09 20:37:56 +0000

    Add theme toggle
    Implement a theme toggle component, allowing users to switch between light,
    dark, and automatic modes, and integrate it into the navigation bar. The
    component uses local storage to save the user's preferred theme and updates
    the document's class list accordingly.
    
    Signed-off-by: John Strunk <john.strunk@gmail.com>

---
+++ src/components/LightDark/LightDark.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import LightDark from './LightDark';
+
+const meta: Meta<typeof LightDark> = {
+    component: LightDark,
+};
+
+export default meta;
+type Story = StoryObj<typeof LightDark>;
+
+export const normal: Story = {
+};
