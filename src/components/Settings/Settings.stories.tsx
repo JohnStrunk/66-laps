f75c7c40b9e1afeba5c509da638dd885e55db48f
John Strunk <john.strunk@gmail.com>
2025-09-22 17:06:17 +0000

    Upgrade storybook to v9
    Used automated upgrade:
    `yarn dlx storybook@latest upgrade`
    
    Signed-off-by: John Strunk <john.strunk@gmail.com>

--- src/components/Settings/Settings.stories.tsx
+++ src/components/Settings/Settings.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { fn } from '@storybook/test';
+import { fn } from 'storybook/test';
 import Settings from './Settings';
 
 const meta: Meta<typeof Settings> = {
